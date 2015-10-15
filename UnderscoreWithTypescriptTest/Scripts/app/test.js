04627fab22dfcb43ebef6c325025a7c23d40db0a
Raphael Lima <rmesquita@gmail.com>
2015-10-14 22:24:27 -0300

    Commit inicial

---
+++ UnderscoreWithTypescriptTest/Scripts/app/test.js
@@ -0,0 +1,18 @@
+/// <reference path="../typings/underscore/underscore.d.ts" />
+/// <reference path="../typings/knockout/knockout.d.ts" />
+var Test;
+(function (Test) {
+    var array = _.uniq([
+        { name: "oi1", id: 1 },
+        { name: "oi3", id: 2 },
+        { name: "oi2", id: 2 },
+        { name: "oi2", id: 1 },
+        { name: "oi2", id: 1 }
+    ], function (value) { return value.id; });
+    console.log(array);
+    var objectWithDate = {
+        Data: ko.observable(new Date(2015, 8, 10))
+    };
+    console.log(ko.mapping.toJSON(objectWithDate));
+})(Test || (Test = {}));
+//# sourceMappingURL=test.js.map
---
+++ UnderscoreWithTypescriptTest/Scripts/app/test.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="../typings/underscore/underscore.d.ts" />
+/// <reference path="../typings/knockout/knockout.d.ts" />
+module Test {
+    var array = _.uniq([
+        { name: "oi1", id: 1 },
+        { name: "oi3", id: 2 },
+        { name: "oi2", id: 2 },
+        { name: "oi2", id: 1 },
+        { name: "oi2", id: 1 }
+    ], (value) => value.id);
+
+    console.log(array);
+
+
+    var objectWithDate = {
+         Data: ko.observable(new Date(2015, 8, 10))
+    };
+
+    console.log(ko.mapping.toJSON(objectWithDate));
+}
