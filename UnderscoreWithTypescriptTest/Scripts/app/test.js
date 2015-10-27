60a37c2c33673dd578b367869252b3ea253f42f4
Raphael Lima <rmesquita@gmail.com>
2015-10-27 21:12:51 -0200

    Como fazer download de arquivo

--- UnderscoreWithTypescriptTest/Scripts/app/test.js
+++ UnderscoreWithTypescriptTest/Scripts/app/test.js
@@ -1,19 +1,10 @@
 /// <reference path="../typings/underscore/underscore.d.ts" />
 /// <reference path="../typings/knockout/knockout.d.ts" />
-// Teste!
-var Test;
-(function (Test) {
-    var array = _.uniq([
-        { name: "oi1", id: 1 },
-        { name: "oi3", id: 2 },
-        { name: "oi2", id: 2 },
-        { name: "oi2", id: 1 },
-        { name: "oi2", id: 1 }
-    ], function (value) { return value.id; });
-    console.log(array);
-    var objectWithDate = {
-        Data: ko.observable(new Date(2015, 8, 10))
-    };
-    console.log(ko.mapping.toJSON(objectWithDate));
-})(Test || (Test = {}));
-//# sourceMappingURL=test.js.map
+/// <reference path="../typings/jquery/jquery.d.ts" />
+$(function () {
+    var myUrl = "http://spatialkeydocs.s3.amazonaws.com/FL_insurance_sample.csv.zip";
+    var $iframe = $('<iframe>/').attr({
+        src: myUrl,
+        style: "visibility:hidden;display:none"
+    }).appendTo($("body"));
+});
--- UnderscoreWithTypescriptTest/Scripts/app/test.ts
+++ UnderscoreWithTypescriptTest/Scripts/app/test.ts
@@ -1,22 +1,35 @@
 ﻿/// <reference path="../typings/underscore/underscore.d.ts" />
 /// <reference path="../typings/knockout/knockout.d.ts" />
+/// <reference path="../typings/jquery/jquery.d.ts" />
 
 // Teste!
 module Test {
-    var array = _.uniq([
-        { name: "oi1", id: 1 },
-        { name: "oi3", id: 2 },
-        { name: "oi2", id: 2 },
-        { name: "oi2", id: 1 },
-        { name: "oi2", id: 1 }
-    ], (value) => value.id);
+    //var array = _.uniq([
+    //    { name: "oi1", id: 1 },
+    //    { name: "oi3", id: 2 },
+    //    { name: "oi2", id: 2 },
+    //    { name: "oi2", id: 1 },
+    //    { name: "oi2", id: 1 }
+    //], (value) => value.id);
 
-    console.log(array);
+    //console.log(array);
 
 
-    var objectWithDate = {
-         Data: ko.observable(new Date(2015, 8, 10))
-    };
+    //var objectWithDate = {
+    //     Data: ko.observable(new Date(2015, 8, 10))
+    //};
 
-    console.log(ko.mapping.toJSON(objectWithDate));
+    //console.log(ko.mapping.toJSON(objectWithDate));
+
+
 }
+
+
+$(() => {
+    var myUrl = "http://spatialkeydocs.s3.amazonaws.com/FL_insurance_sample.csv.zip";
+
+    var $iframe = $('<iframe>/').attr({
+        src: myUrl,
+        style: "visibility:hidden;display:none"
+    }).appendTo($("body"));
+});
